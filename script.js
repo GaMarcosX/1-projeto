d0185d808630acc8d6608c80d05db05dc2218b7d
Mateus Ito <mateusito10@gmail.com>
2023-12-19 09:04:21 -0300

    fix: typo on asset name
    Update dark mode image path to match the asset name
    
    fix #1

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
   }
 }
 
